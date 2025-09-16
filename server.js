98d5f8828b6dfc319b2e87bfd19f6e2c2c8a7895
Brian Sosnowski <sosnowskibrian@gmail.com>
2025-09-16 11:44:29 -0700

    skip static assets

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-#!/usr/bin/env node
+﻿#!/usr/bin/env node
 var prerender = require('./lib');
 var memoryCache = require('prerender-memory-cache');
 
 var server = prerender();
 
 server.use(prerender.sendPrerenderHeader());
+if (process.env.PRERENDER_SKIP_STATIC_ASSETS !== 'false') {
+  server.use(prerender.skipStaticAssets());
+}
 server.use(prerender.browserForceRestart());
 // server.use(prerender.blockResources());
 server.use(prerender.addMetaTags());
